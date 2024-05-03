0b5cc63d1254e208fcf66e2d2be16da00f1ed1da
firyalshafa <shafafiryal782@gmail.com>
2024-05-03 20:20:08 +0700

    membuat prosedur traverse

--- pertemuan9/pertemuan9.cpp
+++ pertemuan9/pertemuan9.cpp
@@ -124,6 +124,22 @@ bool listEmpty()
     return (START == NULL);
 }
 
+void traverse()
+{
+    if (listEmpty())
+        cout << "\nlist is empty" << endl;
+    else
+    {
+        cout << "\nRecords in ascending order of roll number are:" << endl;
+        node* currentNode = START;
+        while (currentNode != NULL)
+        {
+            cout << currentNode->noMhs << "" << currentNode->name << endl;
+            currentNode = currentNode->next;
+        }
+    }
+}
+
 
 
 int main()
